613399d72dc43269ea9d81a10a3314b5a37509a4
lid_leon <leon.liding@huawei.com>
2021-06-24 21:37:21 +0800

    add  my_learning_quant.py

--- my_learning_quant.py
+++ my_learning_quant.py
@@ -33,10 +33,13 @@ np.set_printoptions(precision=5, suppress=True, linewidth=150)
 
 
 # Load data
-def load_data():
-    price = np.arange(200 / 10.0)  # linearly increasing prices
-    return price
+# def load_data():
+#     price = np.arange(200 / 10.0)  # linearly increasing prices
+#     return price
 
+def load_data():
+    price = np.sin(np.arange(200)/30.0) #sine prices
+    return price
 
 # Initialize first state, all items are placed deterministically
 def init_state(data):
@@ -73,13 +76,20 @@ def take_action(state, xdata, action, signal, time_step):
     # move the market data window one step forward
     state = xdata[time_step - 1:time_step, :]
     # take action
-    if action != 0:
-        if action == 1:
-            signal.loc[time_step] = 100
-        elif action == 2:
-            signal.loc[time_step] = -100
-        elif action == 3:
-            signal.loc[time_step] = 0
+    if action < -0.03 :
+        signal.loc[time_step] = -100
+    elif action > 0.03:
+        signal.loc[time_step] = 100
+    else:
+        signal.loc[time_step] = 0
+
+    # if action != 0:
+    #     if action == 1:
+    #         signal.loc[time_step] = 100
+    #     elif action == 2:
+    #         signal.loc[time_step] = -100
+    #     elif action == 3:
+    #         signal.loc[time_step] = 0
     terminal_state = False
 
     return state, time_step, signal, terminal_state
@@ -91,18 +101,22 @@ def get_reward(new_state, time_step, action, xdata, signal, terminal_state, epoc
     signal.fillna(value=0, inplace=True)
     if not terminal_state:
         # get reward for the most current action
-        if signal[time_step] != signal[time_step - 1]:
-            i = 1
-            while signal[time_step - i] == signal[time_step - 1 - i] and time_step - 1 - i > 0:
-                i += 1
-            reward = (xdata[time_step - 1, 0] - xdata[time_step - i - 1, 0]) * signal[
-                time_step - 1] * -100 + i * np.abs(signal[time_step - 1]) / 10.0
-        if signal[time_step] == 0 and signal[time_step - 1] == 0:
-            reward -= 10
+        forwardsetep = min(time_step + 5, len(xdata)-1)
+        # print(f"forward_step{forwardsetep}, timestep{time_step}")
+        reward = (xdata[forwardsetep][0] - xdata[time_step][0])*action
+        # if signal[time_step] != signal[time_step - 1]:
+        #     i = 1
+        #     while signal[time_step - i] == signal[time_step - 1 - i] and time_step - 1 - i > 0:
+        #         i += 1
+            # reward = (xdata[time_step - 1, 0] - xdata[time_step - i - 1, 0]) * signal[
+            #     time_step - 1] * -100 + i * np.abs(signal[time_step - 1]) / 10.0
+        # if signal[time_step] == 0 and signal[time_step - 1] == 0:
+        #     reward -= 10
 
     # calculate the reward for all actions if the last iteration in set
     if terminal_state:
         # run backtest, send list of trade signals and asset data to backtest function
+        print("signal", signal)
         bt = twp.Backtest(pd.Series(data=[x[0] for x in xdata]), signal, signalType='shares')
         reward = bt.pnl.iloc[-1]
 
@@ -145,7 +159,7 @@ model.add(Dense(4, kernel_initializer='lecun_uniform'))
 model.add(Activation('relu'))
 # model.add(Dropout(0.2))
 
-model.add(Dense(4, kernel_initializer='lecun_uniform'))
+model.add(Dense(1, kernel_initializer='lecun_uniform'))
 model.add(Activation('linear'))  # linear output so we can have range of real-valued outputs
 
 rms = RMSprop()
@@ -179,24 +193,25 @@ for i in range(epochs):
         # Run the Q function on S to get predicted reward values on all the possible actions
         qval = model.predict(state.reshape(1, 2), batch_size=1)
         if (random.random() < epsilon) and i != epochs - 1:  # maybe choose random action if not the last epoch
-            action = np.random.randint(0, 4)  # assumes 4 different actions
+            # action = np.random.randint(0, 4)  # assumes 4 different actions
+            action = np.random.rand(1)
         else:  # choose best action from Q(s,a) values
-            action = (np.argmax(qval))
+            action = qval
         # Take action, observe new state S'
         new_state, time_step, signal, terminal = take_action(state, xdata, action, signal, time_step)
         # Observe reward
         reward = get_reward(new_state, time_step, action, xdata, signal, terminal, i)
         # Get max_Q(S',a)
-        newQ = model.predict(new_state.reshape(1, 2), batch_size=1)
-        maxQ = np.max(newQ)
-        y = np.zeros((1, 4))
+        # newQ = model.predict(new_state.reshape(1, 2), batch_size=1)
+        # maxQ = np.max(newQ)
+        y = np.zeros((1, 1))
         y[:] = qval[:]
         if not terminal:  # non-terminal state
             # update = (reward + (gamma * maxQ))
-            update = (1 - alpha) * update + alpha * (reward + (gamma * maxQ))
+            update = (1 - alpha) * update + alpha * (reward + (gamma * 0))
         else:  # terminal state (means that it is the last state)
             update = reward
-        y[0][action] = update  # target output
+        y[0][0] = update  # target output
         model.fit(state.reshape(1, 2), y, batch_size=1, epochs=1, verbose=0)
         state = new_state
     eval_reward = evaluate_Q(indata, model)
